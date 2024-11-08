7acc64a8c035cf4a3605f247ee4d2d11c359725b
이아름 <areum6321@gmail.com>
2024-11-08 15:07:46 +0900

    feat: Todo Update 기능 구현

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,17 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const onChangeCheckBox = () => {
+    onUpdate(id);
+  };
   return (
     <div className="TodoItem">
-      <input readOnly checked={isDone} type="checkBox" />
+      <input
+        onChange={onChangeCheckBox}
+        readOnly
+        checked={isDone}
+        type="checkBox"
+      />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
